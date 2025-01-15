f253fd2f875e9cee588609626dbb5829e89ceb8b
Timon Dubreuil <timondubreuil@outlook.com>
2025-01-15 11:05:23 +0100

    setup driver write data

--- Driver/app/src/main/java/com/example/miniuberdriver/MainActivity.kt
+++ Driver/app/src/main/java/com/example/miniuberdriver/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.example.miniuberdriver
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.database
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +21,12 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World 2 !")
+
+        Log.d("MyApp","Inserted data !")
     }
 }
